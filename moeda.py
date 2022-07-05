7b03ab0edd06316fc51c1743b01a04949640481f
Flag-fix <isac.bandeir4@gmail.com>
2022-07-05 16:42:54 -0300

    Envio correcao

---
+++ moeda.py
@@ -0,0 +1,19 @@
+from objeto import Objeto
+from pygame import image, sprite, transform
+
+
+class Moeda(Objeto):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = image.load('img/moeda.png')
+        self.rect = self.image.get_rect()
+        self.rect[0] = x
+        self.rect[1] = y
+        
+    def update(self, chao_velocidade):
+        self.rect.x -= chao_velocidade
+        if self.rect.right < 0:
+            self.kill()
+        
+        
+    
