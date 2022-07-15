d8bdd01fd7f659c9e8c9611e414bd9913b4b9f64
mateus barbeiro <mateus_garcia01@hotmail.com>
2022-07-15 14:50:50 -0300

    equipe

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from game import Game
 
+# EQUIPE
+# Adler Mateus Cachuba
+# Franciel Ruam Ferreira Santos 
+# Isac Ponciano Bandeira
+# Mateus Barbeiro Garcia
+
 if __name__ == '__main__':
     game = Game()
     game.start_game()
